6e58bf1d8c1f4706da67fb37414e6a3cfab87534
RonBendet <bendetron@gmail.com>
2020-10-24 19:54:57 +0300

    DHProject | Fix Route

--- Backend/src/routes/ocr-routs.ts
+++ Backend/src/routes/ocr-routs.ts
@@ -2,6 +2,6 @@ import { getData, updateImageData, uploadImage } from '../controllers/ocr';
 
 export default function setRoutes({ app }: any) {
   app.post('/', async (req: any, res: any) => uploadImage({ req, res }));
-  app.get('/', async (req: any, res: any) => getData({ req }));
+  app.get('/', async (req: any, res: any) => getData({ req, res }));
   app.put('/', async (req: any, res: any) => updateImageData({ req, res }));
 }
